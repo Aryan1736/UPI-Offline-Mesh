cf85c5d3d8d38ef0b5744a8bc46c69add6fc4b19
Aryan1736 <aryanbagchi17@gmail.com>
2026-05-31 13:05:29 +0530

    Initial Spring Boot setup

---
+++ UpiMesh/src/main/java/com/demo/UpiMesh/UpiMeshApplication.java
@@ -0,0 +1,13 @@
+package com.demo.UpiMesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiMeshApplication.class, args);
+	}
+
+}
---
+++ UpiMesh/src/test/java/com/demo/UpiMesh/UpiMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.UpiMesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiMeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
